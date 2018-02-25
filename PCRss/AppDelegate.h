506a7510ebbb0f9b54cdc8680a4c0390090484e9
Sterling <sterling.li@gmail.com>
2018-02-24 21:45:28 -0800

    Initial Commit

---
+++ PCRss/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PCRss
+//
+//  Created by Zhiye Li on 2/24/18.
+//  Copyright © 2018 Sterling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PCRss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PCRss
+//
+//  Created by Zhiye Li on 2/24/18.
+//  Copyright © 2018 Sterling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
